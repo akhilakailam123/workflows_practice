80c4e89c8ddd80860faf610cd104b98fc531a2da
kailam <kailamakhila@gmail.com>
2023-08-29 12:24:11 +0530

    changes done

--- features/lambda_function_code.py
+++ features/lambda_function_code.py
@@ -1,5 +1,5 @@
-def handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': 'Hello from my Lambda function!'
-    }
+# def handler(event, context):
+#     return {
+#         'statusCode': 200,
+#         'body': 'Hello from my Lambda function!'
+#     }
--- features/steps/lambda_function_steps.py
+++ features/steps/lambda_function_steps.py
@@ -1,30 +1,30 @@
-import os
-
-import boto3
-from behave import given,when
-
-
-@given('My aws account is created')
-def aws_account(context):
-    context.client = boto3.client('lambda')
-
-
-@when('I create a Lambda function named "{function_name}"')
-def step_impl(context, function_name):
-    context.function_name = function_name
-    function_code_path = os.path.join(os.path.dirname(__file__), '../lambda_function_code.py')
-    with open(function_code_path, 'rb') as code_file:
-        function_code = code_file.read()
-    response = context.client.create_function(
-        FunctionName=function_name,
-        Runtime='python3.8',
-        Role='arn:aws:iam::997609234031:role/lambda-execution-role',
-        Handler='lambda_function.handler',
-        Code={
-            'ZipFile': function_code
-        },
-        Timeout=10,
-        MemorySize=128
-    )
-    context.function_arn = response['FunctionArn']
-
+# import os
+#
+# import boto3
+# from behave import given,when
+#
+#
+# @given('My aws account is created')
+# def aws_account(context):
+#     context.client = boto3.client('lambda')
+#
+#
+# @when('I create a Lambda function named "{function_name}"')
+# def step_impl(context, function_name):
+#     context.function_name = function_name
+#     function_code_path = os.path.join(os.path.dirname(__file__), '../lambda_function_code.py')
+#     with open(function_code_path, 'rb') as code_file:
+#         function_code = code_file.read()
+#     response = context.client.create_function(
+#         FunctionName=function_name,
+#         Runtime='python3.8',
+#         Role='arn:aws:iam::997609234031:role/lambda-execution-role',
+#         Handler='lambda_function.handler',
+#         Code={
+#             'ZipFile': function_code
+#         },
+#         Timeout=10,
+#         MemorySize=128
+#     )
+#     context.function_arn = response['FunctionArn']
+#
